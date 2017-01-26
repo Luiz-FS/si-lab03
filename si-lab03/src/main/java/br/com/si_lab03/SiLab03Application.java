d61966ec7b87b21f6dcf70b367f482e336c493e7
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-01-26 14:59:06 -0300

    Dependencia terminadas

---
+++ si-lab03/src/main/java/br/com/si_lab03/SiLab03Application.java
@@ -0,0 +1,12 @@
+package br.com.si_lab03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiLab03Application {
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(SiLab03Application.class, args);
+    }
+}
