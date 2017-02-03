81dbc6c3528b79edefe912c27da54e35bb3ae12a
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-02-03 11:35:22 -0300

    alterando idea

---
+++ si-lab03/src/test/java/br/com/si_lab03/SiLab03ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.si_lab03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiLab03ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
